279fda3b69d08017f36c4a0644ff7d332d6d8298
FelipeArruda <felipe@oktagon.com.br>
2021-06-04 23:17:56 -0300

    change? Added simple Movement script

---
+++ Game-Jam-Unity/Assets/Scripts/GamePlay/PlayerRunner.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Pun.Demo.Asteroids;
+using Photon.Pun.UtilityScripts;
+using UnityEngine;
+
+public class PlayerRunner : MonoBehaviour
+{
+    public float RotationSpeed = 90.0f;
+    public float MovementSpeed = 2.0f;
+    public float MaxSpeed = 0.2f;
+
+    public ParticleSystem Destruction;
+    public GameObject EngineTrail;
+    public GameObject BulletPrefab;
+
+    private PhotonView photonView;
+
+#pragma warning disable 0109
+    private new Rigidbody rigidbody;
+    private new Collider collider;
+    private new Renderer renderer;
+#pragma warning restore 0109
+
+    private float rotation = 0.0f;
+    private float acceleration = 0.0f;
+    private float shootingTimer = 0.0f;
+
+    private bool controllable = true;
+
+    #region UNITY
+
+    public void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        renderer = GetComponent<Renderer>();
+    }
+
+    public void Start()
+    {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.color = AsteroidsGame.GetColor(photonView.Owner.GetPlayerNumber());
+        }
+    }
+
+    public void Update()
+    {
+        if (!photonView.IsMine || !controllable)
+        {
+            return;
+        }
+
+        rotation = Input.GetAxis("Horizontal");
+        acceleration = Input.GetAxis("Vertical");
+
+        if (Input.GetButton("Jump") && shootingTimer <= 0.0)
+        {
+            shootingTimer = 0.2f;
+
+            photonView.RPC("Fire", RpcTarget.AllViaServer, rigidbody.position, rigidbody.rotation);
+        }
+
+        if (shootingTimer > 0.0f)
+        {
+            shootingTimer -= Time.deltaTime;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        if (!controllable)
+        {
+            return;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        rigidbody.velocity = new Vector3(horizontal, 0, vertical) * MovementSpeed * Time.fixedTime;
+
+        if (rigidbody.velocity.magnitude > (MaxSpeed * 1000.0f))
+        {
+            rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed * 1000.0f;
+        }
+    }
+
+    #endregion
+
+    #region COROUTINES
+
+    private IEnumerator WaitForRespawn()
+    {
+        yield return new WaitForSeconds(AsteroidsGame.PLAYER_RESPAWN_TIME);
+
+        photonView.RPC("RespawnSpaceship", RpcTarget.AllViaServer);
+    }
+
+    #endregion
+    
+    
+}
