1aca570724c2afd6552f443ffb42ba1a7d7bf5a7
rafael <raafael.mooraes@gmail.com>
2021-06-05 14:20:48 -0300

    Added AOE input selector.

---
+++ Game-Jam-Unity/Assets/Scripts/GamePlay/TargetMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    public bool CanMove { get; set; }
+    public float MovementSpeed = 2.0f;
+    private Rigidbody m_rigibody;
+    private void Awake()
+    {
+        m_rigibody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!CanMove)
+        {
+            return;
+        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        float diagonalMultiplier = horizontal != 0 && vertical != 0 ? 0.75f : 1f;
+        m_rigibody.velocity = new Vector3(horizontal, 0, vertical) * MovementSpeed * Time.fixedDeltaTime * diagonalMultiplier;
+    }
+}
