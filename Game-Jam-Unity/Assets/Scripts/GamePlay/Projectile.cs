8a107143096fac4ed0db828aea61f73855b0014e
Leonardo Garbelotti <leo13garbelotti@hotmail.com>
2021-06-06 01:42:51 -0300

    archer projectiles are now a parabola

--- Game-Jam-Unity/Assets/Scripts/GamePlay/Projectile.cs
+++ Game-Jam-Unity/Assets/Scripts/GamePlay/Projectile.cs
@@ -1,32 +1,62 @@
-using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using GG.Constants;
+using Photon.Pun;
+using Photon.Pun.Demo.Asteroids;
+using Photon.Pun.UtilityScripts;
 using UnityEngine;
+using System;
 
 public class Projectile : MonoBehaviour 
 {
     public float speed;
+
+    private float t = 0;
+
     private Transform player;
-    private Vector2 target;
+    private Vector3 target;
+
+    private Vector3 start;
 
     void Start(){
         player = GameObject.FindWithTag("Player").transform;
 
-        target = new Vector2(player.position.x, player.position.y);
+        target = player.position;
+        start = transform.position;
+        
     }
 
     void Update() {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        t += Time.deltaTime * speed * 0.1f;
+
+        transform.position = Projectile.Parabola(start, target, 3, t);
         
         if(transform.position.x == target.x && transform.position.y == target.y){
             DestroyProjectile();
         }
     }
 
-    void OnTriggerEnter(Collider other){
-        if(other.CompareTag("Player")){
-            DestroyProjectile();
+    public static Vector3 Parabola(Vector3 start, Vector3 end, float height, float t)
+    {
+        Func<float, float> f = x => -4 * height * x * x + 4 * height * x;
+
+        var mid = Vector3.Lerp(start, end, t);
+
+        return new Vector3(mid.x, f(t) + Mathf.Lerp(start.y, end.y, t), mid.z);
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (GetComponent<PhotonView>().IsMine)
+            {
+                collision.gameObject.GetComponent<PhotonView>().RPC("LoseLife", RpcTarget.All);
+                DestroyProjectile();
+            }
         }
     }
+
     void DestroyProjectile(){
         Destroy(gameObject);
     }
